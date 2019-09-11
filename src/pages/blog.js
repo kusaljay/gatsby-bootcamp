9e4542b52148fdfacc47ce0165fc2d61db1e7005
da_kfernando <da_kfernando@ahpra.gov.au>
2019-09-11 16:01:37 +1000

    source file system, markdown plugins

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import Layout from '../components/layout/layout';
+import { graphql, useStaticQuery } from 'gatsby';
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  console.log(data);
+  //console.log(data.allMarkdownRemark.edges);
+
+  const postsList = data.allMarkdownRemark.edges.map(el => {
+    return (
+      <li>
+        <h2>{el.node.frontmatter.title}</h2>
+        <p>{el.node.frontmatter.date}</p>
+      </li>
+    );
+  });
+
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>Blog posts will appear here.</p>
+      <ol>
+        {postsList}
+      </ol>
     </Layout>
   )
 }
