05ad39a93a71cda2fbb5f1f4dc17fde8d6791ece
kusaljay <kufe28@gmail.com>
2020-06-04 17:06:22 +1000

    Add custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout/layout';
+
+const PageNotFound = () => {
+  return (
+    <Layout>
+      Page not found.
+    </Layout>
+  );
+}
+
+export default PageNotFound;
